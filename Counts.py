96b1a3b2eb4531dfce9ef57e28ca785d61eacc3b
Ibrahim Murtaza <ibrahim.murtaza24@gmail.com>
2021-10-18 21:26:10 +0500

    Reading csv files

---
+++ Counts.py
@@ -0,0 +1,10 @@
+# Importing Libraries
+import pandas as pd
+import numpy as np
+
+# Loading annotations file
+annot = pd.read_csv('via_project_31Aug2021_13h49m_csv.csv')
+# To print the annotations
+print(annot)
+
+

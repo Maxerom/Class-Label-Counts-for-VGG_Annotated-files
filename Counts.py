8c0e6e24d27fd721d3ef720273cc1ef1e65142ff
Ibrahim Murtaza <ibrahim.murtaza24@gmail.com>
2021-10-18 21:27:45 +0500

    Reading classes in annotations file

--- Counts.py
+++ Counts.py
@@ -7,4 +7,7 @@ annot = pd.read_csv('via_project_31Aug2021_13h49m_csv.csv')
 # To print the annotations
 print(annot)
 
+# Reading class attributes column
+clas = annot["region_attributes"]
+#print(clas)
 
